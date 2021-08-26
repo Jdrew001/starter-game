8714a5c22043ce2dbbfce1f6ee6500ff44920a25
Drew Atkison <dtatkison@gmail.com>
2021-08-26 16:46:24 -0500

    added decorations

--- src/consts/TextureKeys.ts
+++ src/consts/TextureKeys.ts
@@ -1,4 +1,9 @@
 export enum TextureKeys {
     Background = 'background',
-    RocketMouse = 'rocket-mouse'
+    RocketMouse = 'rocket-mouse',
+    MouseHole = 'mouse-hole',
+    Window1 = 'window-1',
+    Window2 = 'window-2',
+    Bookcase1 = 'bookcase-1',
+    Bookcase2 = 'bookcase-2'
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -11,14 +11,18 @@ export default class Preloader extends Phaser.Scene {
 
     preload() {
         this.load.image(TextureKeys.Background, 'house/bg_repeat_340x640.png');
-
+        this.load.image(TextureKeys.MouseHole, 'house/object_mousehole.png');
+        this.load.image(TextureKeys.Window1, 'house/object_window1.png');
+        this.load.image(TextureKeys.Window2, 'house/object_window2.png');
+        this.load.image(TextureKeys.Bookcase1, 'house/object_bookcase1.png');
+        this.load.image(TextureKeys.Bookcase2, 'house/object_bookcase2.png');
         this.load.atlas(TextureKeys.RocketMouse, 'characters/rocket-mouse.png', 'characters/rocket-mouse.json');
     }
     
     create() {
         this.anims.create({
             key: Animationkeys.RocketMouseRun,
-            frames: this.anims.generateFrameNames('rocket-mouse', {
+            frames: this.anims.generateFrameNames(TextureKeys.RocketMouse, {
                 start: 1,
                 end: 4,
                 prefix: 'rocketmouse_run',
